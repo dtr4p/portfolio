3743c9ab71d4c7843dbf72f69b7fa5b41f99102e
dtr4p <152822486+dtr4p@users.noreply.github.com>
2024-03-09 17:39:36 -0300

    portfolio v2.0

--- src/Componentes/elements/Card.js
+++ src/Componentes/elements/Card.js
@@ -1,16 +1,30 @@
 import style from './Card.module.css'
-import Lpdnc from '../../Images/projects/lpdnc.svg'
 import ButtonB from './ButtonB'
+import {useState} from 'react'
 
 function Card({title, techs, link, image}){
+
+    const [info, setInfo] = useState(false)
+    function showInfo(){
+        setInfo(true)
+    };
+    function hideInfo(){
+        setInfo(false)
+    }
+
+
     return(
-        <div className={style.Cards}>
-            <img src={image}/>
-            <div className={style.desc}>
-                <h3>{title}</h3>
-                <p><strong>Tecnologias:</strong></p><p>{techs}</p>
-                <a target='_blank' href={link}><ButtonB text='Acesse o repositório'></ButtonB></a>
-            </div>
+        <div onMouseLeave={hideInfo} className={style.Cards}>
+            <a target='_blank' href={link}><img onMouseEnter={showInfo} src={image}/></a>
+
+            {info === true &&(
+                        <div onMouseEnter={showInfo} className={style.desc}>
+                            <h3>{title}</h3>
+                            <p><strong>Tecnologias:</strong></p><p>{techs}</p>
+                            <a target='_blank' href={link}><ButtonB text='Ver projeto'></ButtonB></a>
+                        </div>
+            )}
+
         </div>
     )
 }
